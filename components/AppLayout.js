1490c18bbfcfba1eda72fb0f5cccbe0c86e4ea93
maldives0 <maliethy@gmail.com>
2021-01-18 19:08:34 +0900

    fix project list img height at xl size & update menu close when click layout field

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,173 +1,181 @@
 import React, { useCallback, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Layout, Menu, Typography, Space, Row, Tooltip } from 'antd';
-import { default as BulbOutlined, } from '@ant-design/icons/BulbOutlined';
-import { default as UserOutlined, } from '@ant-design/icons/UserOutlined';
-import { default as AppstoreAddOutlined, } from '@ant-design/icons/AppstoreAddOutlined';
-import { default as BankOutlined, } from '@ant-design/icons/BankOutlined';
-import { default as FormOutlined, } from '@ant-design/icons/FormOutlined';
-import { default as MailOutlined, } from '@ant-design/icons/MailOutlined';
-import { default as HomeOutlined, } from '@ant-design/icons/HomeOutlined';
-import { default as MinusCircleTwoTone, } from '@ant-design/icons/MinusCircleTwoTone';
-import { default as PlusCircleTwoTone, } from '@ant-design/icons/PlusCircleTwoTone';
-import { GlobalLayout, Logo, } from './style';
+import { default as BulbOutlined } from '@ant-design/icons/BulbOutlined';
+import { default as UserOutlined } from '@ant-design/icons/UserOutlined';
+import { default as AppstoreAddOutlined } from '@ant-design/icons/AppstoreAddOutlined';
+import { default as BankOutlined } from '@ant-design/icons/BankOutlined';
+import { default as FormOutlined } from '@ant-design/icons/FormOutlined';
+import { default as MailOutlined } from '@ant-design/icons/MailOutlined';
+import { default as HomeOutlined } from '@ant-design/icons/HomeOutlined';
+import { default as MinusCircleTwoTone } from '@ant-design/icons/MinusCircleTwoTone';
+import { default as PlusCircleTwoTone } from '@ant-design/icons/PlusCircleTwoTone';
+import { GlobalLayout, Logo } from './style';
 import { FullPage } from 'react-full-page';
-import { useMediaQuery } from "react-responsive";
+import { useMediaQuery } from 'react-responsive';
 import Image from 'next/image';
 
 const { Header, Sider, Footer, Content } = Layout;
 const { Text } = Typography;
 const collapsedStyle = {
-    position: 'fixed', top: 60, width: 150, zIndex: 100, left: '14px', border: 'none',
+  position: 'fixed',
+  top: 60,
+  width: 150,
+  zIndex: 100,
+  left: '14px',
+  border: 'none',
 };
 const basicStyle = {
-    position: 'fixed', bottom: '15%', left: 0, width: 200, zIndex: 100,
+  position: 'fixed',
+  bottom: '15%',
+  left: 0,
+  width: 200,
+  zIndex: 100,
 };
 
-const CustomControls = (collapsed, setCollapsed) => ({ getCurrentSlideIndex, scrollToSlide }) => {
-    const currentSlideIndex = getCurrentSlideIndex();
-    const onClickMenu = useCallback((e) => {
-        scrollToSlide(e.key - 1);
-    }, []);
-    const [currentStyle, setCurrentStyle] = useState(basicStyle);
-    const isTabletPC = useMediaQuery({ query: "(min-width: 768px) " });
+const CustomControls = (collapsed, setCollapsed) => ({
+  getCurrentSlideIndex,
+  scrollToSlide,
+}) => {
+  const currentSlideIndex = getCurrentSlideIndex();
+  const onClickMenu = useCallback((e) => {
+    scrollToSlide(e.key - 1);
+  }, []);
+  const [currentStyle, setCurrentStyle] = useState(basicStyle);
+  const isTabletPC = useMediaQuery({ query: '(min-width: 768px) ' });
 
-    useEffect(() => {
-        if (!isTabletPC) {
-            setCurrentStyle({
-                display: 'none',
-            });
-        } else {
-            setCurrentStyle(basicStyle);
-            setCollapsed(false);
-        }
-    }, [isTabletPC]);
-    return (
-        <>
-            <Menu theme="light" mode="inline"
-                selectedKeys={[`${currentSlideIndex + 1}`]}
-                onClick={onClickMenu}
-                style={collapsed ? collapsedStyle : currentStyle}
-            >
-                <Menu.Item key="1" icon={
-                    <HomeOutlined />}>
-                    Home
-                </Menu.Item>
-                <Menu.Item key="2" icon={
-                    <UserOutlined />}>
-                    About
-                </Menu.Item>
-                <Menu.Item key="3" icon={
-                    <AppstoreAddOutlined />}>
-                    Tech Skills
-                </Menu.Item>
-                <Menu.Item key="4" icon={
-                    <BulbOutlined />
-                }>
-                    Project
-                </Menu.Item>
-                <Menu.Item key="5" icon={
-                    <FormOutlined />
-                }>
-                    Experience
-                </Menu.Item>
-                <Menu.Item key="6" icon={
-                    <BankOutlined />}>
-                    Education
-                </Menu.Item>
-                <Menu.Item key="7" icon={
-                    <MailOutlined />
-                }>
-                    Contact
-                </Menu.Item>
-            </Menu>
-        </>
-    );
+  useEffect(() => {
+    if (!isTabletPC) {
+      setCurrentStyle({
+        display: 'none',
+      });
+    } else {
+      setCurrentStyle(basicStyle);
+      setCollapsed(false);
+    }
+  }, [isTabletPC]);
+  return (
+    <>
+      <Menu
+        theme="light"
+        mode="inline"
+        selectedKeys={[`${currentSlideIndex + 1}`]}
+        onClick={onClickMenu}
+        style={collapsed ? collapsedStyle : currentStyle}
+      >
+        <Menu.Item key="1" icon={<HomeOutlined />}>
+          Home
+        </Menu.Item>
+        <Menu.Item key="2" icon={<UserOutlined />}>
+          About
+        </Menu.Item>
+        <Menu.Item key="3" icon={<AppstoreAddOutlined />}>
+          Tech Skills
+        </Menu.Item>
+        <Menu.Item key="4" icon={<BulbOutlined />}>
+          Project
+        </Menu.Item>
+        <Menu.Item key="5" icon={<FormOutlined />}>
+          Experience
+        </Menu.Item>
+        <Menu.Item key="6" icon={<BankOutlined />}>
+          Education
+        </Menu.Item>
+        <Menu.Item key="7" icon={<MailOutlined />}>
+          Contact
+        </Menu.Item>
+      </Menu>
+    </>
+  );
 };
 const AppLayout = ({ children }) => {
-    const isTabletPC = useMediaQuery({ query: "(min-width: 768px)" });
-    const [collapsed, setCollapsed] = useState(false);
-    const toggleCollapsed = useCallback(() => {
-        setCollapsed((prev) => !prev);
-    }, []);
+  const isTabletPC = useMediaQuery({ query: '(min-width: 768px)' });
+  const [collapsed, setCollapsed] = useState(false);
+  const toggleCollapsed = useCallback(() => {
+    setCollapsed((prev) => !prev);
+  }, []);
+  const closeCollapsed = useCallback(() => {
+    setCollapsed(false);
+  }, []);
 
-    return (
-        <>
-            {isTabletPC ? (
-                <Layout className="layout">
-                    <GlobalLayout />
-                    <Sider
-                        prefecth="false"
-                        collapsible
-                        collapsed={collapsed}
-                        trigger={null}
-                        className="sider-layout">
-                        <Logo>
-                            <Row gutter={3} style={{ display: 'block' }}>
-                                <Image
-                                    src="/img/profile-me.jpg"
-                                    alt="profile"
-                                    width={150}
-                                    height={150}
-                                />
-                            </Row>
-                            <Row gutter={3}>
-                                <Space
-                                    style={{ fontSize: 13 }}
-                                    direction="vertical">
-                                    <>
-                                        <Text type="secondary">Juyoung Jung</Text>
-                                        <Text strong>Front-End Developer</Text>
-                                    </>
-                                </Space>
-                            </Row>
-                        </Logo>
-                    </Sider >
-                    <Layout style={{ overflow: 'unset' }}>
-                        <Content>
-                            <div>
-                                <FullPage
-                                    controls={CustomControls(collapsed, setCollapsed)}
-                                >
-                                    {children}
-                                </FullPage>
-                            </div>
-                        </Content>
-                        <Footer>
-                            <p>Juyoung's Portpolio ©2020 Created by Juyoung Jung</p></Footer>
-                    </Layout>
-                </Layout>
-            ) : (
-                    <Layout className="layout">
-                        <GlobalLayout />
-                        <Header className="layout-background">
-                            <Tooltip
-                                placement="right"
-                                title={collapsed ? "메뉴 감추기" : "메뉴 보기"}
-                                className="menufold-layout">
-                                {React.createElement(collapsed ? MinusCircleTwoTone : PlusCircleTwoTone, {
-                                    className: 'trigger',
-                                    onClick: toggleCollapsed,
-                                })}
-                            </Tooltip>
-                        </Header>
-                        <Content>
-                            <div>
-                                <FullPage
-                                    controls={CustomControls(collapsed, setCollapsed)}
-                                >
-                                    {children}
-                                </FullPage>
-                            </div>
-                        </Content>
-                        <Footer>
-                            <p>Juyoung's Portpolio ©2020 Created by Juyoung Jung</p></Footer>
-                    </Layout>
-                )}
-        </>
-    );
+  return (
+    <>
+      {isTabletPC ? (
+        <Layout className="layout">
+          <GlobalLayout />
+          <Sider
+            prefecth="false"
+            collapsible
+            collapsed={collapsed}
+            trigger={null}
+            className="sider-layout"
+          >
+            <Logo>
+              <Row gutter={3} style={{ display: 'block' }}>
+                <Image
+                  src="/img/profile-me.jpg"
+                  alt="profile"
+                  width={150}
+                  height={150}
+                />
+              </Row>
+              <Row gutter={3}>
+                <Space style={{ fontSize: 13 }} direction="vertical">
+                  <>
+                    <Text type="secondary">Juyoung Jung</Text>
+                    <Text strong>Front-End Developer</Text>
+                  </>
+                </Space>
+              </Row>
+            </Logo>
+          </Sider>
+          <Layout style={{ overflow: 'unset' }}>
+            <Content>
+              <div>
+                <FullPage controls={CustomControls(collapsed, setCollapsed)}>
+                  {children}
+                </FullPage>
+              </div>
+            </Content>
+            <Footer>
+              <p>Juyoung's Portpolio ©2020 Created by Juyoung Jung</p>
+            </Footer>
+          </Layout>
+        </Layout>
+      ) : (
+        <Layout className="layout">
+          <GlobalLayout />
+          <Header className="layout-background">
+            <Tooltip
+              placement="right"
+              title={collapsed ? '메뉴 감추기' : '메뉴 보기'}
+              className="menufold-layout"
+            >
+              {React.createElement(
+                collapsed ? MinusCircleTwoTone : PlusCircleTwoTone,
+                {
+                  className: 'trigger',
+                  onClick: toggleCollapsed,
+                },
+              )}
+            </Tooltip>
+          </Header>
+          <Content onClick={closeCollapsed}>
+            <div>
+              <FullPage controls={CustomControls(collapsed, setCollapsed)}>
+                {children}
+              </FullPage>
+            </div>
+          </Content>
+          <Footer>
+            <p>Juyoung's Portpolio ©2020 Created by Juyoung Jung</p>
+          </Footer>
+        </Layout>
+      )}
+    </>
+  );
 };
 AppLayout.propTypes = {
-    children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
 export default AppLayout;
--- components/Project.js
+++ components/Project.js
@@ -1,132 +1,160 @@
 import React, { useState, useCallback } from 'react';
-import { Typography, Card, Row, Col, List, Descriptions, Button, Modal, } from 'antd';
+import {
+  Typography,
+  Card,
+  Row,
+  Col,
+  List,
+  Descriptions,
+  Button,
+  Modal,
+} from 'antd';
 import { default as GithubOutlined } from '@ant-design/icons/GithubOutlined';
 import { default as SelectOutlined } from '@ant-design/icons/SelectOutlined';
 import { ProjectLayout } from './style';
 import { default as CoffeeOutlined } from '@ant-design/icons/CoffeeOutlined';
 import projectData from '../assets/projectData';
-import { useMediaQuery } from "react-responsive";
+import { useMediaQuery } from 'react-responsive';
 import Image from 'next/image';
 
-const { Link, Title, } = Typography;
+const { Link, Title } = Typography;
 const Project = () => {
-    const isTabletPC = useMediaQuery({ query: "(min-width: 768px)" });
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [modalKey, setModalKey] = useState('daon');
-    const toggleModal = useCallback((key) => () => {
-        setIsModalVisible(true);
-        setModalKey(key);
-    }, []);
-    const handleCancel = useCallback(() => {
-        setIsModalVisible(false);
-    }, []);
-    const seletedData = projectData.find(v => v.key === modalKey);
-    return (
+  const isTabletPC = useMediaQuery({ query: '(min-width: 768px)' });
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [modalKey, setModalKey] = useState('daon');
+  const toggleModal = useCallback(
+    (key) => () => {
+      setIsModalVisible(true);
+      setModalKey(key);
+    },
+    [],
+  );
+  const handleCancel = useCallback(() => {
+    setIsModalVisible(false);
+  }, []);
+  const seletedData = projectData.find((v) => v.key === modalKey);
+  return (
+    <>
+      <div
+        data-aos="fade-down"
+        data-aos-delay="200"
+        data-aos-anchor="#trigger-project"
+      >
+        <ProjectLayout />
+        <Row justify={isTabletPC ? 'space-around' : 'center'}>
+          <List
+            grid={{
+              xs: 2,
+              sm: 2,
+              md: 2,
+              lg: 4,
+              xl: 4,
+              xxl: 4,
+              gutter: [8, 8],
+            }}
+            dataSource={projectData}
+            itemLayout="horizontal"
+            renderItem={(item) => (
+              <List.Item>
+                <Col>
+                  <Card
+                    hoverable
+                    onClick={toggleModal(item.key)}
+                    cover={
+                      <Image
+                        src={item.img}
+                        alt={item.key}
+                        width={70}
+                        height={120}
+                      />
+                    }
+                  >
+                    {isTabletPC ? (
+                      <Card.Meta title={item.title} description={item.desc} />
+                    ) : (
+                      <Title level={5}>{item.title}</Title>
+                    )}
+                  </Card>
+                </Col>
+              </List.Item>
+            )}
+          />
+        </Row>
         <>
-            <div data-aos="fade-down"
-                data-aos-delay="200"
-                data-aos-anchor="#trigger-project"
+          <Modal
+            width={1000}
+            height={1000}
+            bodyStyle={{ display: 'flex' }}
+            onCancel={handleCancel}
+            footer={[
+              <Button key="live">
+                <Link prefetch="false" href={seletedData.live} target="_blank">
+                  <SelectOutlined style={{ marginRight: 2 }} />
+                  바로가기
+                </Link>
+              </Button>,
+              <Button key="github" type="primary">
+                <Link prefetch="false" href={seletedData.git} target="_blank">
+                  <GithubOutlined style={{ marginRight: 2 }} />
+                  코드보기
+                </Link>
+              </Button>,
+            ]}
+            title={seletedData.title}
+            visible={isModalVisible}
+          >
+            {isTabletPC ? (
+              <div className="modal-image-style">
+                <Image
+                  src={seletedData.img}
+                  alt={seletedData.key}
+                  width={300}
+                  height={450}
+                />
+              </div>
+            ) : null}
+            <Descriptions
+              layout={'vertical'}
+              bordered={true}
+              size={'small'}
+              column={{ xs: 2, sm: 2, md: 1 }}
             >
-                <ProjectLayout />
-                <Row justify={isTabletPC ? "space-around" : "center"} >
-                    <List
-                        grid={{
-                            xs: 2,
-                            sm: 2,
-                            md: 2,
-                            lg: 4,
-                            xl: 4,
-                            gutter: [8, 8],
-                        }}
-                        dataSource={projectData}
-                        itemLayout="horizontal"
-                        renderItem={(item) => (
-                            <List.Item>
-                                <Col>
-                                    <Card
-                                        hoverable
-                                        onClick={toggleModal(item.key)}
-                                        cover={<Image
-                                            src={item.img}
-                                            alt={item.key}
-                                            width={70} height={120}
-                                        />} >
-                                        {isTabletPC ? (<Card.Meta title={item.title}
-                                            description={item.desc}
-                                        />) : (<Title
-                                            level={5}>{item.title}</Title>)}
-                                    </Card>
-                                </Col>
-                            </List.Item>
-                        )}
-                    />
-                </Row>
-                <>
-                    <Modal
-                        width={1000}
-                        height={1000}
-                        bodyStyle={
-                            { display: 'flex' }
-                        }
-                        onCancel={handleCancel}
-                        footer={[
-                            <Button key="live">
-                                <Link
-                                    prefetch="false"
-                                    href={seletedData.live} target="_blank">
-                                    <SelectOutlined style={{ marginRight: 2 }} />
-                                    바로가기</Link>
-                            </Button>,
-                            <Button key="github" type="primary">
-                                <Link
-                                    prefetch="false"
-                                    href={seletedData.git} target="_blank">
-                                    <GithubOutlined style={{ marginRight: 2 }} />
-                                    코드보기</Link>
-                            </Button>
-
-                        ]}
-                        title={seletedData.title}
-                        visible={isModalVisible} >
-                        {isTabletPC ? (<div className="modal-image-style">
-                            <Image
-                                src={seletedData.img}
-                                alt={seletedData.key}
-                                width={300}
-                                height={450}
-                            />
-                        </div>) : null}
-                        <Descriptions
-                            layout={"vertical"}
-                            bordered={true}
-                            size={"small"}
-                            column={{ xs: 2, sm: 2, md: 1 }}
-                        >
-                            <Descriptions.Item label="Term">{seletedData.term}</Descriptions.Item>
-                            <Descriptions.Item label="Using Skills">{seletedData.skills}</Descriptions.Item>
-                            <Descriptions.Item label="func">{seletedData.func}</Descriptions.Item>
-                            <Descriptions.Item label="detailed description">{seletedData.details}</Descriptions.Item>
-                        </Descriptions>
-                    </Modal>
-                </>
-                <Row xs={16} sm={12} md={8} className="project-title-layout">
-                    <Title
-                        data-aos="fade-up"
-                        data-aos-duration="2000"
-                        level={5}
-                    >이와 같은 프로젝트를 만들며 제가 공부했던 과정이 궁금하시다면 저의  개인 블로그에 놀러와주세요~
-                </Title>
-                </Row>
-                <Row >
-                    <Button
-                        type="primary"
-                        key="blog" className="blog-button-layout" >
-                        <Link href="https://maldives0.github.io" target="_blank" prefetch="false"> 블로그 놀러가기<CoffeeOutlined />
-                        </Link>
-                    </Button>
-                </Row>
-            </div>
+              <Descriptions.Item label="Term">
+                {seletedData.term}
+              </Descriptions.Item>
+              <Descriptions.Item label="Using Skills">
+                {seletedData.skills}
+              </Descriptions.Item>
+              <Descriptions.Item label="func">
+                {seletedData.func}
+              </Descriptions.Item>
+              <Descriptions.Item label="detailed description">
+                {seletedData.details}
+              </Descriptions.Item>
+            </Descriptions>
+          </Modal>
         </>
-    );
-}
+        <Row xs={16} sm={12} md={8} className="project-title-layout">
+          <Title data-aos="fade-up" data-aos-duration="2000" level={5}>
+            이와 같은 프로젝트를 만들며 제가 공부했던 과정이 궁금하시다면 저의
+            개인 블로그에 놀러와주세요~
+          </Title>
+        </Row>
+        <Row>
+          <Button type="primary" key="blog" className="blog-button-layout">
+            <Link
+              href="https://maldives0.github.io"
+              target="_blank"
+              prefetch="false"
+            >
+              {' '}
+              블로그 놀러가기
+              <CoffeeOutlined />
+            </Link>
+          </Button>
+        </Row>
+      </div>
+    </>
+  );
+};
 export default Project;
